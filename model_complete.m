c3ad2a55e18a39b715b5d6d32753e5742c4de8b0
Klemen Košir <klemen.kosir@kream.io>
2015-12-22 21:02:46 +0100

    Initial commit.

---
+++ model_complete.m
@@ -0,0 +1,65 @@
+% dP ... diferencial proteinov
+function dP = model_complete(P)
+    % geni so shranjeni v globalni spremenljivki
+    G = getGlobalx;
+
+	% m ... stevilo genov v enacbi    
+	m = size(G, 1);
+	dP = zeros(1, size(P, 1));
+
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    %%%% STRUKTURA GENA %%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    % G(i,1)  ... nacin izrazanja
+    %             (0 = gensko, 1 = linearna mod., 2 = encimska mod)
+    % G(i,2)  ... parameter alfa/beta
+    % G(i,3)  ... indeks prvega proteina (P2 pri modifikaciji)
+	% G(i,4)  ... mejna koncentracija prvega proteina (P2 pri modifikaciji)
+	% G(i,5)  ... indeks drugega proteina
+	% G(i,6)  ... mejna koncentracija drugega proteina
+	% G(i,7)  ... nacin degradacije
+    %             (0 = linearna, 1 = encimska, 2 = aktivna)
+	% G(i,8)  ... parameter delta
+    % TODO: 9 in 10 lahko zdruzimo.
+	% G(i,9)  ... koncentracija, pri kateri je razgradnja polovicna
+	% G(i,10) ... indeks drugega proteina
+    
+    for i = 1 : m 
+        if G(i, 1) == 0 % gensko izrazanje
+            tf1 = 1;
+            tf2 = 1;
+
+            if G(i, 3) > 0 % aktivator
+                tf1 = heaviside(P(G(i, 3)) - G(i, 4));
+            elseif G(i, 3) < 0 % represor
+                tf1 = heaviside(G(i, 4) - P(-G(i, 3)));
+            end
+
+            if G(i, 5) > 0 % aktivator
+                tf2 = heaviside(P(G(i, 5)) - G(i, 6));
+            elseif G(i, 5) < 0 % represor
+                tf2 = heaviside(G(i, 6) - P(-G(i, 5)));
+            end
+
+            dP(i) = dP(i) + G(i, 2) * tf1 * tf2;
+        elseif G(i, 1) == 1 % linearna modifikacija
+            % Koncentracija trenutnega proteina se zmanjsa.
+            dP(i) = dP(i) - G(i, 2) * P(i);
+            dP(G(i, 3)) = dP(G(i, 3)) + G(i, 2) * P(i);
+        elseif G(i, 1) == 2 % encimska modifikacija
+            dP(i) = dP(i) - G(i, 2) * (P(i) / (G(i, 4) + P(i)));
+            dP(G(i, 3)) = dP(G(i, 3)) + G(i, 2) * (P(i) / (G(i, 4) + P(i)));
+        end
+
+        % TODO: Vhodni protein se ne degradira? if i ~= 1
+        if G(i, 7) == 0 % linearna degradacija
+            dP(i) = dP(i) - G(i, 8) * P(i);
+        elseif G(i, 7) == 1 % encimska degradacija
+            dP(i) = dP(i) - G(i, 8) * P(i) / (G(i, 9) + P(i));
+        elseif G(i, 7) == 2 % aktivna degradacija
+            dP(i) = dP(i) - G(i, 8) * P(i) * P(G(i, 10));
+        end
+    end
+    
+	dP = dP';
+end
---
+++ mutation.m
@@ -0,0 +1,196 @@
+function NG = mutation(G, pd, po, pg, plm, dl, de)
+    % G   ... gensko regulatorno omrezje
+	% pd  ... verjetnost dodajanja proteina
+    % po  ... verjetnost odstranitve proteina
+	% pg  ... verjetnost genskega izrazanja
+	% plm ... verjetnost linearne modifikacije
+	% dl  ... verjetnost linearne degradacije
+	% de  ... verjetnost encimske degradacije
+	sP = size(G, 1); % stevilo proteinov
+    
+    % TODO: Verjetno ne zelimo, da se tako veliko sprememb zgodi.
+    %       Najboljse je, da tudi nakljucno izberemo eno izmed njih.
+    % TODO: Verjetno ne zelimo izbrisati izhodnih proteinov. Ali naj bosta
+    %       izhodna proteina vnaprej dolocena ali sta vedno zadnja proteina
+    %       izhodna?
+	
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% sprememba obstojecega parametra %
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	sel = ceil(rand() * (sP - 1) + 1); % ne mutiramo prvega gena
+	params = [2, 4, 6, 8, 9];
+	idx = params(ceil(rand() * length(params)));
+    G(sel, idx) = G(sel, idx) * (rand() * 2);
+	
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% dodajanje novega proteina %
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	if(sP < 10 && rand() <= pd)
+		P = zeros(1, 10);
+        
+        % TODO: Kako naj se nastavi P(2)? Trenutno dobi vrednost med 1 in 100.
+        % TODO: Enako za P(4), P(6), P(8) in P(9) ter drugje.
+        P(2) = rand() * 100 + 1;
+        P(8) = rand() * 100 + 1;
+        
+        % nacin izrazanja
+		res = rand();
+        if(res < pg) % gensko izrazanje
+			P(1) = 0;
+            
+            if(rand() >= 0.5) % prvi transkripcijski faktor
+                P(3) = randsample(setdiff(1 : sP, sel), 1);
+                P(4) = rand() * 100 + 1;
+                if(rand() < 0.5)
+                    P(3) = -P(3);
+                end
+            end
+            
+            if(rand >= 0.5) % drugi transkripcijski faktor
+                P(5) = randsample(setdiff(1 : sP, [abs(P(3)), sel]), 1);
+                P(6) = rand() * 100 + 1;
+                if(rand() < 0.5)
+                    P(5) = -P(5);
+                end
+            end
+		elseif(res < pg + plm) % linearna modifikacija
+			P(1) = 1;
+            P(3) = randsample(setdiff(1 : sP, sel), 1);
+		else % encimska modifikacija
+			P(1) = 2;
+            P(3) = randsample(setdiff(1 : sP, sel), 1);
+            P(4) = rand() * 100 + 1;
+        end
+
+        % nacin degradacije
+		res = rand();
+        if(res < dl) % linearna degradacija
+			P(7) = 0;
+        elseif(res < dl + de) % encimska degradacija
+			P(7) = 1;
+            P(9) = rand() * 100 + 1;
+        else % aktivna degradacija
+			P(7) = 2;
+            P(10) = randsample(setdiff(1 : sP, sel), 1);
+        end	
+
+		G = [G; P];
+	end
+	
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% spreminjanje nacina degradacije %
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % TODO: Ne upostevaj novo dodanega proteina, ce je bil dodan.
+	sel = ceil(rand() * (sP - 1) + 1);
+    G(sel, 8) = rand() * 100 + 1;
+    
+	res = rand();
+	if(res < dl) % linearna degradacija
+		G(sel, 7) = 0;
+	elseif(res < dl + de) % encimska degradacija
+		G(sel, 7) = 1;
+        G(sel, 9) = rand() * 100 + 1;
+	else % aktivna degradacija
+		G(sel, 7) = 2;
+        G(sel, 10) = randsample(setdiff(1 : sP, sel), 1);
+	end	
+	
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% spreminanje nacina generiranja proteina %
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% TODO: Ne upostevaj novo dodanega proteina, ce je bil dodan.
+	sel = ceil(rand() * (sP - 1) + 1);
+    G(sel, 2) = rand() * 100 + 1;
+    
+	res = rand();
+	if(res < pg) % gensko izrazanje
+		G(sel, 1) = 0;
+        
+        if(rand() >= 0.5) % prvi transkripcijski faktor
+            G(sel, 3) = randsample(setdiff(1 : sP, sel), 1);
+            G(sel, 4) = rand() * 100 + 1;
+            if(rand() < 0.5)
+                G(sel, 3) = -G(sel, 3);
+            end
+        end
+            
+        if(rand >= 0.5) % drugi transkripcijski faktor
+            G(sel, 5) = randsample(setdiff(1 : sP, [abs(G(sel, 3)), sel]), 1);
+            G(sel, 6) = rand() * 100 + 1;
+            if(rand() < 0.5)
+                G(sel, 5) = -G(sel, 5);
+            end
+        end
+	elseif(res < pg + plm) % linearna modifikacija
+        G(sel, 1) = 1;
+        G(sel, 3) = randsample(setdiff(1 : sP, sel), 1);
+	else % encimska modifikacija
+        G(sel, 1) = 2;
+        G(sel, 3) = randsample(setdiff(1 : sP, sel), 1);
+        G(sel, 4) = rand() * 100 + 1;
+	end
+    
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% spreminjanje regulatorjev %
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	sel = ceil(rand() * (sP - 1) + 1);
+	if(G(sel, 1) == 0)
+        % TODO: Nakljucno izberi vrsto spremembe.
+		% dodajanje regulatorja
+        if(G(sel, 3) == 0)
+            G(sel, 3) = randsample(setdiff(1 : sP, [abs(G(sel, 5)), sel]), 1); 
+        elseif(G(sel, 5) == 0)
+			G(sel, 5) = randsample(setdiff(1 : sP, [abs(G(sel, 3)), sel]), 1); 
+        end
+        
+        % brisanje regulatorja
+        % TODO: Lahko odstranimo regulator, tudi ce je samo eden
+        %       -> brezpogojno izrazanje.
+        if(G(sel, 3) ~= 0 && G(sel, 5) ~= 0)
+			G(sel, randsample([3, 5], 1)) = 0;
+        end
+        
+		% spreminjanje vrste regulatorja
+        if(rand() >= 0.5)
+			G(sel, 3) = -G(sel, 3);
+        end	
+        if(rand() >= 0.5)
+			G(sel, 5) = -G(sel, 5);
+        end			
+	end
+    
+	%%%%%%%%%%%%%%%%%%%%%%%%
+	% odstranitev proteina %
+	%%%%%%%%%%%%%%%%%%%%%%%%
+	% TODO: Namesto spremembe lahko parametre tudi odstranimo
+    %       (pri tem je potrebno spremeniti nacin npr. degradacije).
+	if(sP > 3 && rand() < po)
+		sel = ceil(rand() * (sP - 1) + 1);
+		
+        for i = 1 : sP
+            if sel ~= i
+                if(abs(G(i, 3)) == sel)
+                    G(sel, 3) = randsample(setdiff(1 : sP, [sel, abs(G(i, 5)), i]), 1);
+                    if(G(i,1) == 0 && rand() >= 0.5)
+                        G(sel, 3) = -G(sel, 3);
+                    end
+                end
+
+                if(abs(G(i, 5)) == sel)
+                    G(sel, 5) = randsample(setdiff(1 : sP, [sel, abs(G(i, 3)), i]), 1);
+                    if(rand() >= 0.5)
+                        G(sel, 5) = -G(sel,5);
+                    end
+                end
+
+                if(abs(G(i, 10)) == sel)
+                    G(sel, 10) = randsample(setdiff(1 : sP, [sel, i]), 1);
+                end
+            end
+        end
+        
+		[G, ~] = removerows(G, 'ind', sel);
+	end
+    
+    NG = G;
+end
