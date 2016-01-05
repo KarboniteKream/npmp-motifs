a8870e261035048645345d7579f93eca94b7c420
Klemen Košir <klemen.kosir@kream.io>
2016-01-05 15:45:31 +0100

    Pravilna mutacija in selekcija.

--- find_model.m
+++ find_model.m
@@ -6,9 +6,9 @@ function find_model
     
     % create initial population
     % creates cell array of empty matrices
-    pop_array = cell(1, pop_size); %cell array
+    pop_array = cell(1, pop_size * 2); %cell array
     % protein concentrations
-    p_con = cell(1, pop_size);
+    p_con = cell(1, pop_size * 2);
     for i = 1 : pop_size
         % zacetno stevilo proteinov
         M = ones(3,10);
@@ -23,16 +23,12 @@ function find_model
     end
     
     % create learning examples
-    % sinus s frekvenco 10 Hz s 1000 vzorci.
-    x = sin(10 * (0 : 0.01 : 9.99));
-    y_ampl = zeros(size(x));
-    for i = 1 : size(y_ampl, 2)
-        for j = 1 : i
-            y_ampl(1, i) = y_ampl(1, i) + x(j);
-        end
-        y_ampl(1, i) = y_ampl(1, i) / i;
-    end
-    y_per = 0; % TODO
+    % TODO: Dodaj vhodni signal v osebke kot prvi protein.
+    t = 0 : 0.01 : 9.99;
+    f = 10;
+    x = sin(f * t);
+    y_ampl = mean(x) * ones(size(x));
+    y_per = ((2 * pi) / f) * ones(size(x));
 
     % set other parameters (mutation probabilities)
     ppar = 0;
@@ -46,16 +42,28 @@ function find_model
     dl = 0;
     de = 0;
 
+    % cost posameznih osebkov
+    C = zeros(1, pop_size * 2);
+
+    % glavna zanka
     for iter = 1 : max_iterations
-        C = zeros(1, pop_size);
+        % mutacija - dodamo 20 novih osebkov
         for i = 1 : pop_size
-            mutation(pop_array{i}, ppar, pd, pdeg, pgen, preg, po, pg, plm, dl, de);
+            pop_array{20 + i} = mutation(pop_array{i}, ppar, pd, pdeg, pgen, preg, po, pg, plm, dl, de);
+        end
+
+        % simulacija
+        for i = 1 : (pop_size * 2)
             setGlobalx(pop_array{i});
             [~, y] = ode15s(@model_complete, [0, 10 - 0.01], p_con{i});
-            % zadnja dva stolpca v y sta resitvi?
-            C(1, i) = cost(y(:, end - 1), y_ampl) + cost(y(:, end), y_per);
+            C(1, i) = cost(y(:, 2), y_ampl) + cost(y(:, 3), y_per);
         end
+
         % select best
+        [~, sort_idx] = sort(C);
+        pop_array{1 : pop_size} = pop_array{sort_idx(1 : pop_size)};
     end
-    %select best motif
+
+    % select best motif
+    pop_array{1}
 end
